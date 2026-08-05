ba4ba052a2ac072e0aec50e08b573c8451c9921b
nprimo <primo.niccolo@gmail.com>
2026-08-05 11:16:46 +0100

    add graph

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import EnergyChart from "$lib/components/EnergyChart.svelte";
+</script>
+
+<h1>E-REDES consumption</h1>
+
+<EnergyChart />
